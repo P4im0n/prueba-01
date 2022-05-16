3a20d7a8176a5ace5ac79d94c598667f8230add2
Norabuena Rojas Diego Alonso <sdiego-2000@hotmail.com>
2022-05-16 16:39:58 -0500

    Subir proyecto

---
+++ src/gui/Principal.java
@@ -0,0 +1,123 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Principal extends JFrame implements ActionListener {
+
+	private JPanel contentPane;
+	private JMenuBar menuBar;
+	private JMenu mnArchivo;
+	private JMenu mnMantenimiento;
+	private JMenu mnVentas;
+	private JMenu mnConfiguracion;
+	private JMenu mnAyuda;
+	private JMenuItem mntmSalir;
+	private JMenuItem mntmModificarPerfume;
+	private JMenuItem mntmListarPerfumes;
+	private JMenuItem mntmVender;
+	private JMenuItem mntmGenerarReportes;
+	private JMenuItem mntmConfigurarDescuentos;
+	private JMenuItem mntmConfigurarObsequio;
+	private JMenuItem mntmConfigurarCantidadOptimadePerfumesVendidos;
+	private JMenuItem mntmConfigurarPremioSorpresa;
+	private JMenuItem mntmAcercadeTienda;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal frame = new Principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Principal() {
+		setTitle("Tienda 1.0");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 574, 421);
+		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		mnArchivo = new JMenu("Archivo");
+		menuBar.add(mnArchivo);
+		
+		mntmSalir = new JMenuItem("Salir");
+		mntmSalir.addActionListener(this);
+		mnArchivo.add(mntmSalir);
+		
+		mnMantenimiento = new JMenu("Mantenimiento");
+		menuBar.add(mnMantenimiento);
+		
+		JMenuItem mntmConsultarPerfume = new JMenuItem("Consultar perfume");
+		mnMantenimiento.add(mntmConsultarPerfume);
+		
+		mntmModificarPerfume = new JMenuItem("Modificar perfume");
+		mnMantenimiento.add(mntmModificarPerfume);
+		
+		mntmListarPerfumes = new JMenuItem("Listar perfumes");
+		mnMantenimiento.add(mntmListarPerfumes);
+		
+		mnVentas = new JMenu("Ventas");
+		menuBar.add(mnVentas);
+		
+		mntmVender = new JMenuItem("Vender");
+		mnVentas.add(mntmVender);
+		
+		mntmGenerarReportes = new JMenuItem("Generar reportes");
+		mnVentas.add(mntmGenerarReportes);
+		
+		mnConfiguracion = new JMenu("Configuraci\u00F3n");
+		menuBar.add(mnConfiguracion);
+		
+		mntmConfigurarDescuentos = new JMenuItem("Configurar descuentos");
+		mnConfiguracion.add(mntmConfigurarDescuentos);
+		
+		mntmConfigurarObsequio = new JMenuItem("Configurar obsequio");
+		mnConfiguracion.add(mntmConfigurarObsequio);
+		
+		mntmConfigurarCantidadOptimadePerfumesVendidos = new JMenuItem("Configurar cantidad \u00F3ptima de perfumes vendidos");
+		mnConfiguracion.add(mntmConfigurarCantidadOptimadePerfumesVendidos);
+		
+		mntmConfigurarPremioSorpresa = new JMenuItem("Configurar premio sorpresa");
+		mnConfiguracion.add(mntmConfigurarPremioSorpresa);
+		
+		mnAyuda = new JMenu("Ayuda");
+		menuBar.add(mnAyuda);
+		
+		mntmAcercadeTienda = new JMenuItem("Acerca de Tienda");
+		mnAyuda.add(mntmAcercadeTienda);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == mntmSalir) {
+			actionPerformedMntmSalir(e);
+		}
+	}
+	protected void actionPerformedMntmSalir(ActionEvent e) {
+		dispose();
+	}
+}
